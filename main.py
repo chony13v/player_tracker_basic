2e39f661ba71335c3eeb2f4b6302c37b0ca7bfc5
Fausto Vasconez <faustovasconez@Faustos-Laptop.local>
2025-05-23 21:12:05 -0500

    Added interpolation for ball tracks

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from utils import read_video, save_video
 from trackers import PlayerTracker, BallTracker
-from drawers import (PlayerTracksDrawer, BallTracksDrawer)
+from drawers import PlayerTracksDrawer, BallTracksDrawer
 
 
 def main():
@@ -16,16 +16,23 @@ def main():
         video_frames, read_from_stub=True, stub_path="stubs/player_track_stubs.pkl"
     )
 
-    ball_tracks = ball_tracker.get_object_tracks(video_frames, read_from_stub=True, stub_path="stubs/ball_track_stubs.pkl")
+    ball_tracks = ball_tracker.get_object_tracks(
+        video_frames, read_from_stub=True, stub_path="stubs/ball_track_stubs.pkl"
+    )
+
+    # Remove wrong ball Detections
+    ball_tracks = ball_tracker.remove_wrong_detections(ball_tracks)
+    # Interpolate ball tracks
+    ball_tracks = ball_tracker.interpolate_ball_positions(ball_tracks)
 
     # Draw Output
     # Initialize Drawers
-    player_tracks_drawer= PlayerTracksDrawer()
-    ball_tracks_drawer= BallTracksDrawer()
+    player_tracks_drawer = PlayerTracksDrawer()
+    ball_tracks_drawer = BallTracksDrawer()
 
     # Draw Object Tracks
-    output_video_frames= player_tracks_drawer.draw(video_frames, player_tracks)
-    output_video_frames= ball_tracks_drawer.draw(output_video_frames, ball_tracks)
+    output_video_frames = player_tracks_drawer.draw(video_frames, player_tracks)
+    output_video_frames = ball_tracks_drawer.draw(output_video_frames, ball_tracks)
 
     # Save video
     save_video(output_video_frames, "output_videos/output_video.avi")
